4b33fd3e3a7857857a50f04745c1a420ecb7ba3d
Denis Lysenko <un1audd@yandex.ru>
2026-04-12 19:18:24 +0300

    fix run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -12,8 +12,7 @@ fi
 echo "[run.sh] Starting backend on port 9000"
 # Запускаем бэкенд в фоне
 /app/bin/app &
-BACKEND_PID=$!
 
 echo "[run.sh] Starting Caddy on port 8080"
-# Запускаем Caddy (он будет проксировать на :9000)
-exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+# Используем ПОЛНЫЙ путь к бинарнику caddy
+exec /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
